04cab4c172d12bb7676495ff4a5039458985f00d
Th1v4lR <115128159+Th1v4lR@users.noreply.github.com>
2022-11-24 13:12:15 +0700

    Create variabel.java

---
+++ variabel.java
@@ -0,0 +1,11 @@
+public class variabel{
+    public static void main(String[] args){
+        String salam = "Assalamualaikum";
+        String nama = "Thival Rayyani";
+        int lahir = 2003;
+
+        System.out.println(salam);
+        System.out.println("Nama Saya "+nama);
+        System.out.println("Saya Lahir di Tahun : "+lahir);
+    }
+}
